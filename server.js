b843f3fd16b4b42eb51cfca8cc082e841826d724
14-Maisa <93995465+14-Maisa@users.noreply.github.com>
2023-05-27 23:25:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hallo, mein Name ist Maisa');
+res.send('Hallo, das ist die überarbeitete Applikation und mein Name ist Maisa');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
