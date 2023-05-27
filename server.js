30553e505a80e9c66dd10deb18a5b24016e9e3de
14-Maisa <93995465+14-Maisa@users.noreply.github.com>
2023-05-27 20:07:07 +0200

    new files

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+const express = require('express');
+// Constants
+const PORT = 9042;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+res.send('Hallo, mein Name ist Maisa');
+});
+app.listen(PORT, HOST);
+console.log(`Running on
+http://${HOST}:${PORT}`);
